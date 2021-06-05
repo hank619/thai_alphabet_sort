26fad7a6537ad1fec4cb64283f082c8661d3582a
zhanghong <hong.zhang@axinan.com>
2021-06-05 23:47:21 +0800

    init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import styles from './index.less';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+    </div>
+  );
+}
+
+
+function test() {
+  let i = 2;
+  const j = i++;
+}
